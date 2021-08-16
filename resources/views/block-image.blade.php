9a6b082aa66db94108797b22ff3559e68623232b
Dominik <domin1992@users.noreply.github.com>
2021-08-16 23:05:37 +0200

    Fix stdobject

--- resources/views/block-image.blade.php
+++ resources/views/block-image.blade.php
@@ -1,5 +1,5 @@
 <figure class="block-image{{ ($data['withBorder'] ? ' block-image--with-border' : '') }}{{ ($data['withBackground'] ? ' block-image--with-background' : '') }}{{ ($data['stretched'] ? ' block-image--stretched' : '') }}">
-<img src="{{ $data['file']['url'] }}" class="block-image__image" @if(isset($data['caption'])) alt="{{ strip_tags($data['caption']) }}" @endif>
+<img src="{{ $data['file']->url }}" class="block-image__image" @if(isset($data['caption'])) alt="{{ strip_tags($data['caption']) }}" @endif>
 @if(isset($data['caption']) && $data['caption'])
 <figcaption class="block-image__caption">{!! $data['caption'] !!}</figcaption>
 @endif
